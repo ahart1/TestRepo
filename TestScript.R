b9464b43ceb4e080b697467825c3db623605d1a5
ahart1 <arhart555@gmail.com>
2022-08-19 15:41:33 -0400

    Add YAML for GitHub actions and TestScript.R to source.

--- TestScript.R
+++ TestScript.R
@@ -1,1 +1,9 @@
 # This is an example R script to test README generation
+autoREADME(dirREADME = "TestRepo/AnotherFolder")
+
+# Add steps to set up R environment, load R packages, run my script
+
+# Simpler example: https://blog--simonpcouch.netlify.app/blog/r-github-actions-commit/
+# More complex example (probably what I want): https://www.r-bloggers.com/2020/09/running-an-r-script-on-a-schedule-gh-actions/
+# GitHub documentation: https://docs.github.com/en/actions/learn-github-actions/understanding-github-actions
+
